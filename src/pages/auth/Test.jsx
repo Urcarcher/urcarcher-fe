5817256fe80d24fe38545c05427c4b09b5d63fb5
1nthebleakmidwinter <1nth2bleakmidwinter@gmail.com>
2024-08-08 20:23:04 +0900

    로그인 다듬기

--- src/pages/auth/Test.jsx
+++ src/pages/auth/Test.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getOptions } from '../../services/AuthService';
+import axios from 'axios';
 
 function Test(props) {
+    const [a, SetA] = useState({});
+
+    useEffect(()=> {
+        axios(getOptions('/api/t/test', 'GET', null)).then(
+            (resp)=>{
+                SetA(resp.data);
+            }
+        )
+    }, []);
+    
     return (
         <div>
-            <h1>성공이요..</h1>
+            <h1>{a.memberId}</h1>
         </div>
     );
 }
