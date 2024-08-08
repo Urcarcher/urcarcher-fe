70ab019326c80f4f9fff875e8e4fd92752b33deb
kimyurie <9911dbfl@naver.com>
2024-08-08 10:51:34 +0900

    [Feat]: add card selection page with slider banner

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import logo from './logo.svg';
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import './App.css';
+import Card from './kimyuri/pages/Card';
 
 function App() {
-  const [testStr, setTestStr] = useState('');
-
-  function callback(str) {
-    setTestStr(str);
-  }
-
-  useEffect(
-    ()=>{
-      axios.get('/api/t/test')
-      .then((resp)=>{callback(resp.data)})
-      .catch((err)=>{console.log(err)})
-    }, []
-  );
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> api test {'==>'} {testStr} </p>
-      </header>
+      <Card/>
     </div>
   );
 }
