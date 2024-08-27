94580cd41221d7d752986ffa4b1d85761a1c977d
hswimming <sy_0826@naver.com>
2024-08-27 09:36:24 +0900

    feat : 내역보기 버튼 경로 추가 (2024-08-27, 09:36)

--- src/pages/exchange/ExchangeSetSuccess.jsx
+++ src/pages/exchange/ExchangeSetSuccess.jsx
@@ -43,7 +43,7 @@ function ExchangeSetSuccess(props) {
 
     // 환전 내역
     const inforListHandle = () => {
-        navi("/exchange/history");
+        navi("/exchange/history/card");
     };
 
     return (
