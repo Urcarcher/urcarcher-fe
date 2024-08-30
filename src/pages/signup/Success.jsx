daf9a12cef8b80e6e1c095f7f8403dfbf7c05089
syc98syc <helloworldnewstart98@gmail.com>
2024-08-30 17:56:26 +0900

    signup

--- src/pages/signup/Success.jsx
+++ src/pages/signup/Success.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./signup.css"
+import welcome from 'assets/welcome.gif'
 
 function Success() {
     const navigate = useNavigate();
 
-    const handleButtonClick = () => {
-        navigate('/login');
-    };
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate('/login');
+        }, 3000); // 3초 후에 자동으로 로그인 페이지로 이동
+
+        return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+    }, [navigate]);
+
 
     return (
         <div className="contents">
             <div className="mb-4 card">
-                <img className="card-img-top" src="/img/mockup4.jpg" alt="Card image cap" />
+                <img className="card-img-top" src={welcome} alt="welcome image" />
             <div className="card-body">
-            <h1 className="card-text">어카처 회원가입 완료! </h1>
+            <h1 className="card-text">환영합니다!</h1>
+            <h3 className="card-text">어카처 회원이 되었어요</h3>
+            {/* Illustration by <a href="https://icons8.com/illustrations/author/WsYoyZ6jp8sg">Victoria Chepkasova</a> from <a href="https://icons8.com/illustrations">Ouch!</a> */}
             </div>
-            <Button className="my-btn" onClick={handleButtonClick}>어카처 이용하기</Button>
-            {/* <Button className="my-btn" onClick={handleButtonClick}>로그인</Button> */}
             </div>
         </div>
     );
