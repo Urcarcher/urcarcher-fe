87c602eb6dc8c075436ce926de60ed6c167ccdb5
jung hyun <jh441122jh@gmail.com>
2024-09-05 14:06:00 +0900

    Feat: css수정(24.09)

---
+++ src/pages/localProducts/ProductImg.js
@@ -0,0 +1,38 @@
+import img1 from 'assets/localProduct/lp-img1.png';
+import img2 from 'assets/localProduct/lp-img2.png';
+import img3 from 'assets/localProduct/lp-img3.png';
+import img4 from 'assets/localProduct/lp-img4.png';
+import img5 from 'assets/localProduct/lp-img5.png';
+import img6 from 'assets/localProduct/lp-img6.png';
+import img7 from 'assets/localProduct/lp-img7.png';
+import img8 from 'assets/localProduct/lp-img8.png';
+import img9 from 'assets/localProduct/lp-img9.png';
+import img10 from 'assets/localProduct/lp-img10.png';
+import img11 from 'assets/localProduct/lp-img11.png';
+import modalImg1 from 'assets/localProduct/lp-product1.png';
+import modalImg2 from 'assets/localProduct/lp-product2.png';
+import modalImg3 from 'assets/localProduct/lp-product3.png';
+import modalImg4 from 'assets/localProduct/lp-product4.png';
+import modalImg5 from 'assets/localProduct/lp-product5.png';
+import modalImg6 from 'assets/localProduct/lp-product6.png';
+import modalImg7 from 'assets/localProduct/lp-product7.png';
+import modalImg8 from 'assets/localProduct/lp-product8.png';
+import modalImg9 from 'assets/localProduct/lp-product9.png';
+import modalImg10 from 'assets/localProduct/lp-product10.png';
+import modalImg11 from 'assets/localProduct/lp-product11.png';
+
+
+ 
+export const productLinks = [
+    { id: 1, url: "https://m.place.naver.com/place/13345973/home", alt: "망개떡", imgSrc: img1, modalImgSrc: modalImg1 },
+    { id: 2, url: "https://8domall.co.kr/product/공주알밤빵", alt: "알밤빵", imgSrc: img2, modalImgSrc: modalImg2 },
+    { id: 3, url: "https://jayeonwoorimall.com/", alt: "잣한과", imgSrc: img3, modalImgSrc: modalImg3 },
+    { id: 4, url: "https://smartstore.naver.com/applebbang", alt: "사과빵", imgSrc: img4, modalImgSrc: modalImg4 },
+    { id: 5, url: "https://smartstore.naver.com/1000nuri", alt: "비빔빵", imgSrc: img5, modalImgSrc: modalImg5 },
+    { id: 6, url: "https://smartstore.naver.com/ccboy", alt: "생크림찹쌀떡", imgSrc: img6, modalImgSrc: modalImg6 },
+    { id: 7, url: "https://m.place.naver.com/place/13345973/home", alt: "양양샌드", imgSrc: img7, modalImgSrc: modalImg7 },
+    { id: 8, url: "https://hwangnam.com/category/shop/24/", alt: "황남빵", imgSrc: img8, modalImgSrc: modalImg8 },
+    { id: 9, url: "https://naver.me/5JJQDpTg", alt: "성심당시루", imgSrc: img9, modalImgSrc: modalImg9 },
+    { id: 10, url: "https://www.paris.co.kr/promotion/paba-sands/", alt: "마음샌드", imgSrc: img10, modalImgSrc: modalImg10 },
+    { id: 11, url: "https://smartstore.naver.com/thegoodcoffee_tomstoncoffee/products/8399739764#scrollY=1784", alt: "수박커피", imgSrc: img11, modalImgSrc: modalImg11 }
+];
